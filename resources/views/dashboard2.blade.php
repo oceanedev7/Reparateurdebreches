a70e98da64cb8b2793743edf9bdcd63f2521fa04
@jeromezamant972 <jeromezamant@hotmail.com>
2024-07-17 12:41:21 -0400

    page articles

--- resources/views/dashboard2.blade.php
+++ resources/views/dashboard2.blade.php
@@ -10,7 +10,60 @@
 <body>
     <main>
 @section('content')
-{{-- mettre le contenu  --}}
+    <div class="p-5 text-center ">
+
+        <h5 class="mb-2   text-6xl font-bold tracking-tight " style="font-size: 25px; color:#2563EB" >
+            GERER LES ARTICLES</h5>
+    </div>
+    <div class="flex justify-center ">
+         <input class="block w-15 text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400" id="file_input" type="file">
+    </div>
+    <form class="max-w-sm mx-auto">
+        <div>
+            <label for="small-input" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Titre</label>
+            <input type="text" id="small-input" class="block w-full p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 text-xs focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+        </div>
+
+        <div class="mb-5">
+            <label for="large-input" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Description</label>
+            <input type="text" id="large-input" class="block w-full p-4 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 text-base focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+        </div>
+        <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Ajouter</button>
+
+
+    </form>
+
+
+<div class="grid grid-cols-2 md:grid-cols-3 gap-4 justify-items-center ">
+    <div>
+        <img class="h-40 max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image.jpg" alt="">
+        <h5>article 1</h5>
+    </div>
+    <div>
+        <img class="h-40 max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-1.jpg" alt="">
+        <h5>article 2</h5>
+    </div>
+    <div>
+        <img class="h-40 max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-2.jpg" alt="">
+        <h5>article 3</h5>
+    </div>
+    <div>
+        <img class="h-40 max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-3.jpg" alt="">
+        <h5>article 4</h5>
+    </div>
+    <div>
+        <img class="h-40 max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-3.jpg" alt="">
+        <h5>article 5</h5>
+    </div>
+    <div>
+        <img class="h-40 max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-3.jpg" alt="">
+        <h5>article 6</h5>
+    </div>
+</div>
+
+
+
+
 @endsection
     </main>
 
