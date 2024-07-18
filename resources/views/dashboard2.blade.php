3f86eb2caf6fa8f6438c7ffc5d2b85aee70ebecd
@jeromezamant972 <jeromezamant@hotmail.com>
2024-07-18 12:01:25 -0400

    push

--- resources/views/dashboard2.blade.php
+++ resources/views/dashboard2.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.base_admin')
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -12,10 +12,10 @@
 @section('content')
     <div class="p-5 text-center ">
 
-        <h5 class="mb-2   text-6xl font-bold tracking-tight " style="font-size: 25px; color:#2563EB" >
+        <h5 class="mb-2   text-6xl font-bold tracking-wide " style="font-size: 35px; color:#2563EB" >
             GERER LES ARTICLES</h5>
     </div>
-    <div class="flex justify-center ">
+    <div class="flex justify-center mb-9 pr-1 ">
          <input class="block w-15 text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400" id="file_input" type="file">
     </div>
     <form class="max-w-sm mx-auto">
@@ -26,44 +26,46 @@
 
         <div class="mb-5">
             <label for="large-input" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Description</label>
-            <input type="text" id="large-input" class="block w-full p-4 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 text-base focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+            <textarea id="message" rows="4" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Description"></textarea>
         </div>
-        <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Ajouter</button>
+        <div class="flex justify-center mb-5 pr-1">
 
+            <button type="submit" class="  text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Ajouter</button>
+
+        </div>
 
     </form>
 
 
 <div class="grid grid-cols-2 md:grid-cols-3 gap-4 justify-items-center ">
     <div>
-        <img class="h-40 max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image.jpg" alt="">
         <h5>article 1</h5>
+        <img class="h-40 max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image.jpg" alt="">
+
     </div>
     <div>
-        <img class="h-40 max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-1.jpg" alt="">
         <h5>article 2</h5>
+        <img class="h-40 max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-1.jpg" alt="">
     </div>
     <div>
-        <img class="h-40 max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-2.jpg" alt="">
         <h5>article 3</h5>
+        <img class="h-40 max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-2.jpg" alt="">
+
     </div>
     <div>
-        <img class="h-40 max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-3.jpg" alt="">
         <h5>article 4</h5>
+        <img class="h-40 max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-3.jpg" alt="">
     </div>
     <div>
+         <h5>article 5</h5>
         <img class="h-40 max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-3.jpg" alt="">
-        <h5>article 5</h5>
     </div>
     <div>
-        <img class="h-40 max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-3.jpg" alt="">
         <h5>article 6</h5>
+        <img class="h-40 max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-3.jpg" alt="">
+
     </div>
 </div>
-
-
-
-
 @endsection
     </main>
 
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,7 @@ Route::get('/dashboard', function () {
     return view('dashboard2');
 })->middleware(['auth', 'verified'])->name('dashboard2');
 
+
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
