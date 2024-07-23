2c9e2f48a34453e62d87a8360fb13e36c5c46955
oceanedev7 <oceanepy7@gmail.com>
2024-07-22 21:56:30 -0400

    css

--- resources/views/appli/evenement.blade.php
+++ resources/views/appli/evenement.blade.php
@@ -24,9 +24,13 @@
         <a href="{{route('agenda')}}" class="underline text-bleu-fonce"><i class="fa-solid fa-arrow-left underline mt-8 ml-8 text-bleu-fonce"></i> Retour à l'agenda </a>
 
         <div class="w-full flex justify-center">
-            <div class="flex flex-col text-justify p-24 space-y-20 max-w-6xl">
+            <div class="flex flex-col text-justify p-24 space-y-18 max-w-6xl">
                 <div class="space-y-14 text-center">
                     <div class="text-bleu-fonce font-black text-4xl">{{$evenement->titre}}</div>
+                    <div class="flex flex-row text-xl font-bold space-x-12 justify-center">
+                    <div> Date : {{$evenement->date}}</div> 
+                    <div> Lieu : {{$evenement->lieu}} </div>
+                    </div>
                     <div class="text-bleu-fonce text-lg text-justify">
                       {{$evenement->description}}
                     </div>
