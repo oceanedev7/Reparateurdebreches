00d01d14cc1cb7447ba6c6254dbdae3e31245085
@jeromezamant972 <jeromezamant@hotmail.com>
2024-07-23 14:37:23 -0400

    wip

--- resources/views/admin_pages/dashboard_article.blade.php
+++ resources/views/admin_pages/dashboard_article.blade.php
@@ -16,7 +16,7 @@
     @csrf
     <div>
         <label for="small-input" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Titre</label>
-        <input type="text" id="small-input" class="block w-full p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 text-xs focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+        <input type="text" id="small-input" class="block w-full p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 text-xs focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" accept="image/*">
     </div>
 
     <div class="mb-5">
