04d18f3930be564c98dc5edad44a3abf3af9a310
@jeromezamant972 <jeromezamant@hotmail.com>
2024-07-23 14:17:16 -0400

    wip

--- resources/views/admin_pages/dashboard_article.blade.php
+++ resources/views/admin_pages/dashboard_article.blade.php
@@ -13,6 +13,7 @@
      <input class="block w-15 text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400" id="file_input" type="file">
 </div>
 <form class="max-w-sm mx-auto" style="width:500px;" method="post" action="{{route('newArticle')}}">
+    @csrf
     <div>
         <label for="small-input" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Titre</label>
         <input type="text" id="small-input" class="block w-full p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 text-xs focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
@@ -24,7 +25,7 @@
     </div>
     <div class="flex justify-center mb-5 pr-1">
 
-        <button type="submit" class="  text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Ajouter</button>
+        <button  type="submit" class="  text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Ajouter</button>
 
     </div>
 
