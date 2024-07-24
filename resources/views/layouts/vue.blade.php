14046fc30d4d8b355402f6be71051cdde861ea9e
oceanedev7 <oceanepy7@gmail.com>
2024-07-24 17:29:01 -0400

    modif

--- resources/views/layouts/vue.blade.php
+++ resources/views/layouts/vue.blade.php
@@ -37,13 +37,13 @@
 
 
               <!-- Dropdown -->
-              <div class="relative ml-3 sm:hidden">
+              {{-- <div class="relative ml-3 sm:hidden">
                 <button type="button" class="inline-flex items-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:ring-gray-400" id="mobile-menu-button" aria-expanded="false" aria-haspopup="true">
                     <i class="fa-solid fa-bars"></i>
                   <svg class="-mr-1 ml-1.5 h-5 w-5 text-gray-400" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
                     <path fill-rule="evenodd" d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z" clip-rule="evenodd" />
                   </svg>
-                </button>
+                </button> --}}
                 
                 <!--
                   Dropdown menu, show/hide based on menu state.
@@ -55,13 +55,13 @@
                     From: "transform opacity-100 scale-100"
                     To: "transform opacity-0 scale-95"
                 -->
-                <div class=" flex flex-col  absolute right-0 z-10 -mr-1 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="mobile-menu-button" tabindex="-1">
+                {{-- <div class=" flex flex-col  absolute right-0 z-10 -mr-1 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="mobile-menu-button" tabindex="-1"> --}}
                   <!-- Active: "bg-gray-100", Not Active: "" -->
-                  <a class=" text-bleu-fonce text-lg font-bold">Se connecter </a>
+                  {{-- <a class=" text-bleu-fonce text-lg font-bold">Se connecter </a>
                 <a class=" text-bleu-fonce  text-lg font-bold">Devenir adhérent</a>
                 <a class="text-bleu-fonce text-lg font-bold">Faire un don</a>
                 </div>
-              </div>
+              </div> --}}
             </div>
           </div>
         </nav>
