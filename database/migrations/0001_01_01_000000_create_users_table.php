5032662920d4eccb225e7e4c071434c1cb78a7bc
oceanedev7 <oceanepy7@gmail.com>
2024-07-25 11:15:23 -0400

    modif vue articles et all-articles

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -23,7 +23,6 @@ return new class extends Migration {
             $table->string('ville')->nullable();
             $table->string('code_postal')->nullable();
             $table->string('pays')->nullable();
-            $table->string('role')->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password')->default('default_password');
             $table->rememberToken();
